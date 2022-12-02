ed4313196997328dd3a45796dcccd3e91f69056b
Sungpyo <pwsusc10@naver.com>
2022-12-02 12:21:39 +0900

    add play page css

--- src/app.jsx
+++ src/app.jsx
@@ -8,6 +8,7 @@ import Schedule from "./pages/schedule/schedule";
 import Search from "./pages/search";
 import Signup from "./pages/signup";
 import cookies from "react-cookies";
+import Playvideo from "./pages/playvideo";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
           <Route path="/login" exact element={<Login />}></Route>
           <Route path="/login/signup" exact element={<Signup />}></Route>
           <Route path="/homepage" exact element={<Homepage />}></Route>
+          <Route path="/playvideo" exact element={<Playvideo />}></Route>
           <Route path="/schedule" exact element={<Schedule />}></Route>
           <Route path="/search" exact element={<Search />}></Route>
           <Route path="/mypage" exact element={<Mypage />}></Route>
--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import axios from "axios";
+import React, { useState } from "react";
+import { Cookies } from "react-cookie";
 
-const Homepage = (props) => (
-      <div>homepage</div>
+const Homepage = (props) => {
+  const [comment, setComment] = useState({
+    videoID: 1,
+    comment: "abcd",
+  });
+  const cookies = new Cookies();
+  return (
+    <div>
+      <button
+        onClick={() => {
+          console.log("comment", comment);
+          console.log(cookies.get("userID"));
+          axios
+            .post("http://localhost:3001/reply/comment", comment, {
+              headers: {
+                Authorization: `Bearer ${cookies.get("userID")}`,
+              },
+            })
+            .then((v) => {
+              //백엔드 response값
+              console.log(v);
+            })
+            .catch((e) => {
+              console.log("error", e);
+            });
+        }}
+      />
+    </div>
   );
+};
 
 export default Homepage;
